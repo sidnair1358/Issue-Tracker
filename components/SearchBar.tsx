37df339f20cd80086e6c819fea39b585b7b78b96
sidn <siddharth.nair@bravissimo.com>
2024-04-13 22:35:14 +0100

    Create combo box

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,14 +1,18 @@
 "use client";
-
+import { useState } from "react";
 import SearchManufacturer from "./SearchManufacturer";
 
 const SearchBar = () => {
+  const [manufacturer, setManufacturer] = useState<string>("");
   const handleSearch = () => {};
 
   return (
     <form className="searchbar" onSubmit={handleSearch}>
       <div className="searchbar__item">
-        <SearchManufacturer />
+        <SearchManufacturer
+          manufacturer={manufacturer}
+          setManufacturer={setManufacturer}
+        />
       </div>
     </form>
   );
