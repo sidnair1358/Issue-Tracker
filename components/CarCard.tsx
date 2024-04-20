f44bd06f63bff740b94378baf0b631e7ff5e8253
sidn <siddharth.nair@bravissimo.com>
2024-04-20 22:15:32 +0100

    Add styles to card button

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -23,6 +23,7 @@ interface CarProps {
 
 const CarCard = ({ car }: CarProps) => {
   const { city_mpg, make, model, year, drive, transmission } = car;
+  const [isOpen, setIsOpen] = useState<boolean>(false);
 
   const carRent = calculateCarRent(city_mpg, year);
 
@@ -44,8 +45,40 @@ const CarCard = ({ car }: CarProps) => {
           alt="car model"
           fill
           priority
-          className=" object-fill"
+          className="object-contain"
         />
+      </div>
+      <div className="relative flex w-full mt-2">
+        <div className="flex group-hover:invisible w-full justify-between text-gray">
+          <div className="flex flex-col items-center justify-center gap-2">
+            <Image
+              src="/steering-wheel.svg"
+              alt="steering wheel"
+              width={20}
+              height={20}
+            />
+            <p className="text-[14px]">
+              {transmission === "a" ? "Automatic" : "Manual"}{" "}
+            </p>
+          </div>
+          <div className="flex flex-col items-center justify-center gap-2">
+            <Image src="/tire.svg" alt="tire" width={20} height={20} />
+            <p className="text-[14px]">{drive.toUpperCase()}</p>
+          </div>
+          <div className="flex flex-col items-center justify-center gap-2">
+            <Image src="/gas.svg" alt="economy" width={20} height={20} />
+            <p className="text-[14px]">{city_mpg} MPG</p>
+          </div>
+        </div>
+        <div className="car-card__btn-container">
+          <CustomButton
+            title="view more"
+            containerStyles="w-full rounded-full bg-primary-blue py-[16px]"
+            textStyles="text-white text-[14px] leading-[17px] font-bold"
+            rightIcon="/right-arrow.svg"
+            handleClick={() => setIsOpen(true)}
+          />
+        </div>
       </div>
     </div>
   );
--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,11 +1,15 @@
 "use client";
 import React, { MouseEventHandler } from "react";
+import Image from "next/image";
 
 interface Props {
   title: string;
   btnType?: "button" | "submit";
   containerStyles?: string;
+  textStyles?: string;
+  rightIcon?: string;
   handleClick?: MouseEventHandler<HTMLButtonElement>;
+  isDisabled?: boolean;
 }
 
 const CustomButton = (props: Props) => {
@@ -13,10 +17,20 @@ const CustomButton = (props: Props) => {
     <button
       disabled={false}
       type={props.btnType || "button"}
-      className={`custom-btn ${props.containerStyles}`}
+      className={`custom-btn ${props.containerStyles} ${props.textStyles}`}
       onClick={() => {}}
     >
       <span className="flex-1">{props.title}</span>
+      {props.rightIcon && (
+        <div className="relative w-6 h-6">
+          <Image
+            src={props.rightIcon}
+            alt="right icon"
+            fill
+            className="object-contain"
+          />
+        </div>
+      )}
     </button>
   );
 };
